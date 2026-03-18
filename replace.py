5fe6fe9ceb235392d174191e738322149c24e5d2
HOMO-yuxihe <2459379510@qq.com>
2026-03-19 00:08:01 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -4,14 +4,15 @@ for i in os.walk('datapacks'):
     for j in i[2]:
         fp=os.path.join(i[0],j)
         if os.path.splitext(fp)[1] == '.mcfunction':
-            print(fp)
             with open(fp,'r',encoding='utf-8') as f:
                 fc=f.readlines()
                 for index,k in enumerate(fc):
-                    if 'score @s pos' in k:
+                    if '@s dummy' in k or '@s pos' in k:
+                        print(fp)
                         k='$'+k
-                        fc[index]=k.replace('score @s pos','score $(name) pos')
+                        fc[index]=k.replace('@s dummy','$(name) dummy')
+                        fc[index]=fc[index].replace('@s pos','$(name) pos')
             
             with open(fp,'w',encoding='utf-8') as f:
-                f.write('\n'.join(fc))
+                f.write(''.join(fc))
 
